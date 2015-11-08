22d685c882a68ecd7902df9894a2cccb436512fb
gaby <https://Gaby2121@github.com>
2015-11-08 12:39:17 -0600

    Creacion del proyecto

---
+++ inmemdb/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: gaby
+ *
+ * Created on 8 de noviembre de 2015, 12:38 PM
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
